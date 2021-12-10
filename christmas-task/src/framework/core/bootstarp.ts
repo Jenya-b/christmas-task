e09bf5ba6c709b9935e6862d78e671f8fea4af67
jenya-b <evgenyborisevich@inbox.ru>
2021-12-10 14:32:18 +0300

    refactor: apply typescript-eslint/recommended

--- christmas-task/src/framework/core/bootstarp.ts
+++ christmas-task/src/framework/core/bootstarp.ts
@@ -1,5 +1,8 @@
 type moduleType = { start: () => void };
+type EmptyObject = { [K in never]: never };
 
-export function bootstrap(module: {}) {
+export function bootstrap(module: EmptyObject) {
+	console.log(module);
+
 	(module as moduleType).start();
 }
