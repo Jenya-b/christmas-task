a2b63b704c6ab8634ee7a6b804c9cad31ef37939
jenya-b <evgenyborisevich@inbox.ru>
2021-12-12 22:50:39 +0300

    feat: add christmas-decoration page

--- christmas-task/src/app/pages/christmas-decoration/christmas-decoration.ts
+++ christmas-task/src/app/pages/christmas-decoration/christmas-decoration.ts
@@ -8,6 +8,67 @@ class AppChristmasDecoration extends WFMComponent {
 }
 
 export const appChristmasDecoration = new AppChristmasDecoration({
-	selector: 'main',
-	template: ``,
+	selector: 'dinamic-page',
+	template: `
+	<main class="main decoration">
+				<aside class="decoration__sidebar sidebar-decor">
+					<ul class="sidebar-decor__list">
+						<li class="sidebar-decor__item form-toys">
+							<h3 class="sidebar-decor__title">Форма:</h3>
+							<div class="form-toys__wrapper">
+								<button></button>
+								<button></button>
+								<button></button>
+								<button></button>
+								<button></button>
+							</div>
+						</li>
+						<li class="sidebar-decor__item color-toys">
+							<h3 class="sidebar-decor__title">Цвет:</h3>
+							<div class="color-toys__wrapper">
+								<button></button>
+								<button></button>
+								<button></button>
+								<button></button>
+								<button></button>
+							</div>
+						</li>
+						<li class="sidebar-decor__item size-toys">
+							<h3 class="sidebar-decor__title">Размер:</h3>
+							<div class="size-toys__wrapper">
+								<button></button>
+								<button></button>
+								<button></button>
+							</div>
+						</li>
+						<li class="sidebar-decor__item count-toys">
+							<h3 class="sidebar-decor__title">Колличество экземпляров:</h3>
+							<div class="count-toys__wrapper"></div>
+						</li>
+						<li class="sidebar-decor__item year-toys">
+							<h3 class="sidebar-decor__title">Год приобретения:</h3>
+							<div class="year-toys__wrapper"></div>
+						</li>
+						<li class="sidebar-decor__item like-toys">
+							<h3 class="sidebar-decor__title">Только любимые:</h3>
+							<input type="checkbox" />
+						</li>
+					</ul>
+					<button>Сбросить настройки</button>
+				</aside>
+				<article class="decoration__content content-decor">
+					<h1 class="content-decor__title">Бабушкины украшения</h1>
+					<select class="content-decor__select">
+						<option>Пункт 1</option>
+						<option>Пункт 2</option>
+					</select>
+					<section class="content-decor__gallery gallery-toys">
+						<h2 class="gallery-toys__title">Игрушки</h2>
+						<div class="gallery-toys__items">
+							
+						</div>
+					</section>
+				</article>
+			</main>
+	`,
 });
--- christmas-task/src/framework/core/module.ts
+++ christmas-task/src/framework/core/module.ts
@@ -1,6 +1,17 @@
 import { router } from '..';
 import { IComponent } from './component';
 
+interface IData {
+	num: string;
+	name: string;
+	count: string;
+	year: string;
+	shape: string;
+	color: string;
+	size: string;
+	favorite: boolean;
+	length: number;
+}
 type ComponentType = { render: () => void }[];
 type BootstrapComponentType = { render: () => void };
 export type RouteType = {
@@ -41,10 +52,75 @@ export class Module {
 			'dinamic-page'
 		) as HTMLElement).innerHTML = `<${route?.component.selector}></${route?.component.selector}>`;
 		this.renderComponent(route?.component);
+		if (route?.path === 'decor') {
+			this.addToysOnPage();
+		}
 	}
 
 	//! необходимо изменить тип аргумента метода renderComponent()
 	renderComponent(c: any) {
 		c.render();
+	}
+
+	async getInfo(url: string) {
+		const responce = await fetch(url);
+		const data = await responce.json();
+		return data as IData[];
+	}
+
+	async addToysOnPage() {
+		const url = 'https://jenya-b.github.io/json/data.json';
+		const data = await this.getInfo(url);
+		const toysWrapper = document.querySelector('.gallery-toys__items') as HTMLElement;
+
+		for (let i = 0; i < data.length; i++) {
+			toysWrapper.append(...this.getListContent(data[i]));
+		}
+	}
+
+	getListContent(data: IData): HTMLDivElement[] {
+		const result = [];
+
+		const toy = document.createElement('div');
+		const name = document.createElement('h3');
+		const img = document.createElement('img');
+		const ul = document.createElement('ul');
+		const countLi = document.createElement('li');
+		const yearLi = document.createElement('li');
+		const shapeLi = document.createElement('li');
+		const colorLi = document.createElement('li');
+		const sizeLi = document.createElement('li');
+		const favoriteLi = document.createElement('li');
+
+		toy.classList.add('gallery-toys__item', 'toy');
+		name.className = 'toy__name';
+		img.className = 'toy__image';
+		ul.className = 'toy__list';
+		countLi.className = 'toy__item';
+		yearLi.className = 'toy__item';
+		shapeLi.className = 'toy__item';
+		colorLi.className = 'toy__item';
+		sizeLi.className = 'toy__item';
+		favoriteLi.className = 'toy__item';
+
+		countLi.innerText = `Количество: ${data.count}`;
+		yearLi.innerText = `Год покупки: ${data.year}`;
+		shapeLi.innerText = `Форма: ${data.shape}`;
+		colorLi.innerText = `Цвет: ${data.color}`;
+		sizeLi.innerText = `Размер: ${data.size}`;
+		if (data.favorite) {
+			favoriteLi.innerText = `Любимая: да`;
+		} else {
+			favoriteLi.innerText = `Любимая: нет`;
+		}
+		img.src = `https://jenya-b.github.io/json/toys/${data.num}.png`;
+		ul.append(countLi, yearLi, shapeLi, colorLi, sizeLi, favoriteLi);
+
+		name.innerText = `${data.name}`;
+
+		toy.append(name, img, ul);
+		result.push(toy);
+
+		return result;
 	}
 }
